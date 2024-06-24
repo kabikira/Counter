68cd4feda76563b16aca1560d001f90f0ed6972c
kabikira <kabikirar2@protonmail.com>
2024-06-24 23:06:18 +0900

    テストが成功するようリファクタリング

--- Counter/CounterViewController.swift
+++ Counter/CounterViewController.swift
@@ -13,14 +13,21 @@ class CounterViewController: UIViewController {
     @IBOutlet weak var decrementButton: UIButton!
     @IBOutlet weak var incrementButton: UIButton!
 
+    var count: Int = 0
+
     static func make() -> CounterViewController {
         let storyboard = UIStoryboard(name: "Counter", bundle: nil)
         return storyboard.instantiateInitialViewController() as! CounterViewController
     }
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    @IBAction func tapIncrementButton(_ sender: Any) {
+        count += 1
+        countLabel.text = "\(count)"
     }
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        countLabel.text = "\(count)"
+    }
 }
 
